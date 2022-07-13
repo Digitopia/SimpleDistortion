fada45a15ae9254a977ab27b63cdfa3519c95efd
Nuno Hespanhol <nuno.hespanhol@gmail.com>
2022-07-13 19:00:00 +0100

    Init

---
+++ Source/PluginEditor.h
@@ -0,0 +1,44 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class SimpleDistortionAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    SimpleDistortionAudioProcessorEditor (SimpleDistortionAudioProcessor&);
+    ~SimpleDistortionAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    
+    std::unique_ptr<juce::Slider> driveKnob;
+    std::unique_ptr<juce::Slider> rangeKnob;
+    std::unique_ptr<juce::Slider> blendKnob;
+    std::unique_ptr<juce::Slider> volumeKnob;
+    
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> driveAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> rangeAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> blendAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> volumeAttachment;
+    
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    SimpleDistortionAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleDistortionAudioProcessorEditor)
+};
